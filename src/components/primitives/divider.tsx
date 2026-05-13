d44acbdee87fa65122fd2200f3fe6a59dea746ac
mattenarle10 <enarlem10@gmail.com>
2026-05-13 21:42:11 +0800

    remove unused pane and divider primitives

--- src/components/primitives/divider.tsx
+++
@@ -1,7 +0,0 @@
-type DividerProps = {
-  orientation?: "horizontal" | "vertical";
-};
-
-export function Divider({ orientation = "horizontal" }: DividerProps) {
-  return <div className={`mdv-divider mdv-divider--${orientation}`} aria-hidden />;
-}
